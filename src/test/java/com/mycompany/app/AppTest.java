034dfde02b45ba492e8c76027aa203f660cbec04
sivabharathi123 <d.sivabharathi@gmail.com>
2024-04-12 18:05:30 +0100

    Update AppTest.java
    Visibility changed to default

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -10,14 +10,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest
 {
     @Test
-    public void testAppConstructor() {
+    void testAppConstructor() {
         App app1 = new App();
         App app2 = new App();
         assertEquals(app1.getMessage(), app2.getMessage());
     }
 
     @Test
-    public void testAppMessage()
+    void testAppMessage()
     {
         App app = new App();
         assertEquals("Hello World!", app.getMessage());
